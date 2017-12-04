46a343494e670ed20351efa5256f02e0f965fc29
Oleksandr Kushchak <Oleksandr.Kushchak@ibm.com>
2017-12-04 20:31:20 +0000

    add: D1#P2

--- Day_1/Part_1/src.js
+++ Day_1/Part_1/src.js
@@ -6,7 +6,7 @@ let input = helper.getInput('./../input.txt');
 input.push(input[0]); // Append first character to the end
 
 let sum = 0;
-for (var i = 0; i < input.length; i++) {
+for (let i = 0; i < input.length; i++) {
   if (input[i] === input[i+1]) {
     sum += input[i];
   }
--- Day_1/Part_2/src.js
+++ Day_1/Part_2/src.js
@@ -1,6 +1,22 @@
 const fs = require('fs');
 const helper = require('./../helper.js')
 
-let input = helper.getInput('./../input.txt');
+const input = helper.getInput('./../input.txt');
+const half_circle = input.length/2;
+let position, sum = 0;
 
-console.log("This is your captcha: " + input);
+for (let i = 0; i < input.length; i++) {
+
+  if (i >= half_circle) {
+    position = i - half_circle;
+  } else {
+    position = i + half_circle;
+  }
+
+  if (input[i] === input[position]) {
+    sum += input[i];
+  }
+
+}
+
+console.log("This is your captcha: " + sum);
