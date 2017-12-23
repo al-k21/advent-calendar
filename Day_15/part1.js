64915806ad259466a194ae400d2441bd894c8a7e
Oleksandr Kushchak <Oleksandr.Kushchak@ibm.com>
2017-12-23 10:42:29 +0000

    add: D15#P1

---
+++ Day_15/part1.js
@@ -0,0 +1,12 @@
+// Generator A starts with 722
+// Generator B starts with 354
+
+let a = 722, b = 354 , count = 0;
+
+for (let i = 0; i < 40000000 ; i++) {
+  a = (a * 16807) % 2147483647;
+  b = (b * 48271) % 2147483647;
+  if ((a & 0xFFFF) === (b & 0xFFFF)) { count++ }
+}
+
+console.log(count);
