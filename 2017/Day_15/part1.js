3f5dbf375a6eb052de0b02329d3f05ccc25de202
Oleksandr Kushchak <oleksandrkushchak@gmail.com>
2018-03-04 17:12:08 +0000

    add 2016

--- 2017/Day_15/part1.js
+++ 2017/Day_15/part1.js
@@ -6,6 +6,10 @@ let a = 722, b = 354 , count = 0;
 for (let i = 0; i < 40000000 ; i++) {
   a = (a * 16807) % 2147483647;
   b = (b * 48271) % 2147483647;
+  console.log(a,
+    parseInt(a, 2),
+    a & 0xFFFF);
+
   if ((a & 0xFFFF) === (b & 0xFFFF)) { count++ }
 }
 
